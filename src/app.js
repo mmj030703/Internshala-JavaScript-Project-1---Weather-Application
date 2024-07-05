fa504414c583c4f3dd6fc84a9dbbdb61cbd22bd0
mmj030703 <=>
2024-07-05 12:07:18 +0530

    Static design of weather app done

---
+++ src/app.js
@@ -0,0 +1,9 @@
+// Features to be added
+// - search by city name - display weather | == user can do anyone among them
+// - current location - display weather    |
+// - dropdown menu for recently search cities - use localstorage - initially no dropdown
+// - clicking on any city in dropdown should update the weather
+// - Valid user inputs
+// - Functionality to implement extended weather for multiple days - 5 days
+// - Handle API errors gracefully 
+
